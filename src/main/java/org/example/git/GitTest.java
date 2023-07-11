b6096504efabae9a84a7cc1d117a864510404c34
tanzijin <2418049445@qq.com>
2023-07-11 17:02:43 +0800

    commit by hot-fix222

--- src/main/java/org/example/git/GitTest.java
+++ src/main/java/org/example/git/GitTest.java
@@ -5,6 +5,8 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git2!");
         System.out.println("hello git hot-fix!");
+
+        System.out.println("hello git hot-fix22222!");
     }
 }
 
